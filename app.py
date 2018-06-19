5297999fc73ed9694f3caf27f023f37894a590e0
oofin008 <oofpokemon@gmail.com>
2018-06-20 00:08:04 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,88 @@
+﻿from flask import Flask, request
+import json
+import requests
+
+# ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
+global LINE_API_KEY
+# ห้ามลบคำว่า Bearer ออกนะครับเมื่อนำ access token มาใส่
+LINE_API_KEY = 'Bearer YOURSECRETKEY'
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'This is chatbot server.'
+@app.route('/bot', methods=['POST'])
+
+def bot():
+    # ข้อความที่ต้องการส่งกลับ
+    replyQueue = list()
+   
+    # ข้อความที่ได้รับมา
+    msg_in_json = request.get_json()
+    msg_in_string = json.dumps(msg_in_json)
+    
+    # Token สำหรับตอบกลับ (จำเป็นต้องใช้ในการตอบกลับ)
+    replyToken = msg_in_json["events"][0]['replyToken']
+    
+    # ส่วนนี้ดึงข้อมูลพื้นฐานออกมาจาก json (เผื่อ)
+    userID =  msg_in_json["events"][0]['source']['userId']
+    msgType =  msg_in_json["events"][0]['message']['type']
+    
+    # ตรวจสอบว่า ที่ส่งเข้ามาเป็น text รึป่าว (อาจเป็น รูป, location อะไรแบบนี้ได้ครับ)
+    # แต่ก็สามารถประมวลผลข้อมูลประเภทอื่นได้นะครับ
+    # เช่น ถ้าส่งมาเป็น location ทำการดึง lat long ออกมาทำบางอย่าง เป็นต้น
+    if msgType != 'text':
+        reply(replyToken, ['Only text is allowed.'])
+        return 'OK',200
+    
+    # ตรงนี้ต้องแน่ใจว่า msgType เป็นประเภท text ถึงเรียกได้ครับ 
+    # lower เพื่อให้เป็นตัวพิมพ์เล็ก strip เพื่อนำช่องว่างหัวท้ายออก ครับ
+    text = msg_in_json["events"][0]['message']['text'].lower().strip()
+    
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # if text in response_dict:
+    #     replyQueue.append(reponse_dict[text])
+    # else:
+    #     replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
+       
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
+    # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
+    # เพื่อค้นหาประโยคที่ใกล้เคียงที่สุด อาจใช้เรื่องของ word embedding มาใช้งานได้ครับ
+    # simple sentence embeddings --> https://openreview.net/pdf?id=SyK00v5xx
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # closest = find_closest_sentence(response_dict, text)
+    # replyQueue.append(reponse_dict[closest])
+   
+    # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
+    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    
+    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
+    replyQueue.append(msg_in_string)
+    reply(replyToken, replyQueue[:5])
+    
+    return 'OK', 200
+ 
+def reply(replyToken, textList):
+    # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': LINE_API_KEY
+    }
+    msgs = []
+    for text in textList:
+        msgs.append({
+            "type":"text",
+            "text":text
+        })
+    data = json.dumps({
+        "replyToken":replyToken,
+        "messages":msgs
+    })
+    requests.post(LINE_API, headers=headers, data=data)
+    return
+
+if __name__ == '__main__':
+    app.run()
---
+++ ptt_api.py
@@ -0,0 +1,12 @@
+from zeep import Client
+from lxml import etree
+
+client_api = Client('http://www.pttplc.com/webservice/pttinfo.asmx?WSDL')
+result = client_api.service.CurrentOilPrice("en")
+
+Data = etree.fromstring(result)
+
+for Oil in Data.xpath('DataAccess'):
+    product_name = Oil.xpath('PRODUCT/text()')[0]
+    product_price = Oil.xpath('PRICE/text()') or [0]
+    print(product_name, float(product_price[0]),'Baht')
