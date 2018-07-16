ae9afc81a67cc68c58217778dac2b5d6f7ef33bb
oofin008 <oofpokemon@gmail.com>
2018-07-16 13:49:42 +0700

    teesteet

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def bot():
    
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
     replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
-    replayQueue.append(ptt_result)
+    #replayQueue.append(ptt_result)
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
     replyQueue.append(msg_in_string)
     reply(replyToken, replyQueue[:])
