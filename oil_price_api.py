45e132666a8415a9e2479e3b2805f59cc223852b
oofin008 <oofpokemon@gmail.com>
2018-07-17 14:40:49 +0700

    test11

--- oil_price_api.py
+++ oil_price_api.py
@@ -2,12 +2,13 @@ from zeep import Client
 from lxml import etree
 import json
 import requests
+from time import gmtime, strftime
 
 def ptt_result_API():
     client_api = Client('http://www.pttplc.com/webservice/pttinfo.asmx?WSDL')
     result = client_api.service.CurrentOilPrice("en")
     Data = etree.fromstring(result)
-    mystring = str()
+    mystring = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '\n'
     mylist = list()
     for Oil in Data.xpath('DataAccess'):
         product_name = Oil.xpath('PRODUCT/text()')[0]
