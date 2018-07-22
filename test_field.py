b0c65a13b965c0d3d5b40a311ecbec73caa105a6
oofin008 <oofpokemon@gmail.com>
2018-07-22 22:38:01 +0700

    test

--- test_field.py
+++ test_field.py
@@ -1,17 +1,9 @@
 #test DICT python
 
-def main():
-    final_message = []
-    words = ['drat','crap','sucks']
-    counter = 0
-    userInput = input("Enter The Sentense: ")  # use raw_input if you're using python2.X
-    truncatedInput = userInput[:140]
-    sentence =  truncatedInput.split()
-    for word in sentence:
-        if word in words:
-            word = 'x' * len(word)
-        final_message.append(word)
-    print (' '.join(final_message))
+test = ['fick','fuck','fock']
+val = 'fick'
 
-if __name__ == '__main__':
-    main()
+if val in test:
+    print('yoyo')
+else:
+    print('hi')
